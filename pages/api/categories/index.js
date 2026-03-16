e8cd3cef1f5e7f2983f7d07be2b09efcb0552eee
Efraín Sebastián Rojas Artavia <141437780+efrabuilder@users.noreply.github.com>
2026-03-15 19:39:39 -0600

    Delete pages directory

--- pages/api/categories/index.js
+++
@@ -1,5 +0,0 @@
-// pages/api/categories/index.js
-import { getCategories } from "../../../lib/db";
-export default function handler(req, res) {
-  res.json(getCategories());
-}
--- pages/api/report/index.js
+++
@@ -1,5 +0,0 @@
-// pages/api/report/index.js
-import { getReport } from "../../../lib/db";
-export default function handler(req, res) {
-  res.json(getReport());
-}
