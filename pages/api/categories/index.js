0ebdf2b3439d74c0014ca12c7607d97f33caabe9
Efraín Sebastián Rojas Artavia <141437780+efrabuilder@users.noreply.github.com>
2026-03-15 18:24:25 -0600

    Delete pages directory

--- pages/api/categories/index.js
+++
@@ -1,7 +0,0 @@
-// pages/api/categories/index.js
-import { getDb, query } from "../../../lib/db";
-
-export default async function handler(req, res) {
-  const db = await getDb();
-  res.json(query(db, "SELECT * FROM categories ORDER BY name"));
-}
